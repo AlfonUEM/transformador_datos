ab34238250a54126c70313b76fa11a9ecba9c82a
alfon <alfonuem@gmail.com>
2024-05-18 08:52:42 +0200

    Aspecto de cajas de funciones mejorado

--- src/components/DNDFunction.jsx
+++ src/components/DNDFunction.jsx
@@ -7,10 +7,14 @@ import FunctionSettingsModal from "./FunctionSettingsModal";
 
 const FunctionContainer = styled.div`
   border: 1px solid lightgrey;
-  border-radius: 2px;
+  border-radius: 10px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: white;
+  //background-color: #0f1b2a;
+    //background-color: #7d8998;  
+  //background-color: #000716;
+  background-color: #f8f8f8;
+    //color: #fbfbfb;
 `;
 
 export default class DNDFunction extends React.Component {
@@ -21,8 +25,8 @@ export default class DNDFunction extends React.Component {
                     <FunctionContainer {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
                         {this.props.currentColumn === "active_functions_column" ?
                             <Grid gridDefinition={[{colspan: 10}, {colspan: 2}]}>
-                                <div>
-                                {this.props.dndFunction.content}
+                                <div /*style={{color: "#fbfbfb"}}*/>
+                                    {this.props.dndFunction.content}
                                 </div>
                                 <div>
                                     <FunctionSettingsModal dndState={this.props.dndState} functionId={this.props.dndFunction.id} functionName={this.props.dndFunction.content} functionParameters={this.props.dndFunction.jsParameters} updateFunctionParameters={this.props.updateFunctionParameters}/>
