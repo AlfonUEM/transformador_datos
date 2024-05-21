31148058c2dce7913939e8eeea6fd2ddc98c7af2
ManceboJ <jmancebo10@gmail.com>
2024-05-21 22:08:24 +0200

    Ventana modal login
    pendiente funciones para verificar usuarios

--- src/components/MainNavBar.jsx
+++ src/components/MainNavBar.jsx
@@ -1,61 +1,115 @@
 import TopNavigation from "@cloudscape-design/components/top-navigation";
+import React from "react";
+import {Box, Button, Modal, FormField, Input} from "@cloudscape-design/components";
+import SpaceBetween from "@cloudscape-design/components/space-between";
 
 function MainNavBar() {
+
+    const [visible, setVisible] = React.useState(false);
+    const [user, setUser] = React.useState("")
+    const [password, setPassword] = React.useState("")
+
+    const cancelModal = () => {
+        setVisible(false);
+    }
+    const confirmModal = () => {
+        //añadir función que verifique usuario
+        console.log("Se comprueba usuario", user, "y contraseña", password)
+        setVisible(false);
+        window.location.href = '#';
+    }
+
+    const loginClick = (detail) => {
+        if (detail.detail.id === 'login') {
+            setVisible(true);
+        }
+    };
+
     return (
-        <TopNavigation
-            identity={{
-                href: "#",
-                title: "Maestro Codificador",
-                logo: {
-                    src: "/logo-small-top-navigation.svg",
-                    alt: "Maestro Codificador"
+        <>
+            <TopNavigation
+                identity={{
+                    href: "#",
+                    title: "Maestro Codificador",
+                    logo: {
+                        src: "/logo-small-top-navigation.svg",
+                        alt: "Maestro Codificador"
+                    }
+                }}
+                utilities={[
+                    /*{
+                   type: "button",
+                   text: "Link",
+                   href: "https://example.com/",
+                   external: true,
+                   externalIconAriaLabel: " (opens in a new tab)"
+               },
+               {
+                   type: "button",
+                   iconName: "notification",
+                   title: "Notifications",
+                   ariaLabel: "Notifications (unread)",
+                   badge: true,
+                   disableUtilityCollapse: false
+               },
+               {
+                   type: "menu-dropdown",
+                   iconName: "settings",
+                   ariaLabel: "Settings",
+                   title: "Settings",
+                   items: [
+                       {
+                           id: "settings-org",
+                           text: "Organizational settings"
+                       },
+                       {
+                           id: "settings-project",
+                           text: "Project settings"
+                       }
+                   ]
+               },*/
+                    {
+                        type: "menu-dropdown",
+                        text: "Usuario",
+                        description: "Registrese o inicie sesión",
+                        iconName: "user-profile",
+                        items: [
+                            {id: "newaccount", text: "Registrarse", href: "/#createUser"},
+                            {id: "login", text: "Iniciar sesión", onItemClick: loginClick},
+                        ],
+                        onItemClick: loginClick,
+                    }
+                ]}
+            />
+            <Modal
+                onDismiss={() => setVisible(false)}
+                visible={visible}
+                header="Inicio sesión"
+                closeAriaLabel="Cerrar ventana"
+                size="small"
+                footer={
+                    <Box float="right">
+                        <SpaceBetween direction="horizontal" size="xs">
+                            <Button variant="link" onClick={cancelModal}>Cancel</Button>
+                            <Button variant="primary" onClick={confirmModal}>Ok</Button>
+                        </SpaceBetween>
+                    </Box>
                 }
-            }}
-            utilities={[
-                /*{
-                    type: "button",
-                    text: "Link",
-                    href: "https://example.com/",
-                    external: true,
-                    externalIconAriaLabel: " (opens in a new tab)"
-                },
-                {
-                    type: "button",
-                    iconName: "notification",
-                    title: "Notifications",
-                    ariaLabel: "Notifications (unread)",
-                    badge: true,
-                    disableUtilityCollapse: false
-                },
-                {
-                    type: "menu-dropdown",
-                    iconName: "settings",
-                    ariaLabel: "Settings",
-                    title: "Settings",
-                    items: [
-                        {
-                            id: "settings-org",
-                            text: "Organizational settings"
-                        },
-                        {
-                            id: "settings-project",
-                            text: "Project settings"
-                        }
-                    ]
-                },*/
-                {
-                    type: "menu-dropdown",
-                    text: "Usuario",
-                    description: "Registrese o inicie sesión",
-                    iconName: "user-profile",
-                    items: [
-                        {id: "newaccount", text: "Registrarse", href: "/#createUser"},
-                        {id: "login", text: "Iniciar sesión"}
-                    ]
-                }
-            ]}
-        />
+            >
+                <FormField label="Usuario">
+                    <Input type="text"
+                           value={user}
+                           onChange={({detail}) => setUser(detail.value)}/>
+                </FormField>
+                <FormField label="Contraseña">
+                    <Input type="password"
+                           value={password}
+                           onChange={({detail}) => setPassword(detail.value)}/>
+                </FormField>
+            </Modal>
+        </>
     );
 }
+
 
 export default MainNavBar;
